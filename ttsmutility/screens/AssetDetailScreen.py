2b46e5b6d609a616e264219fd6cded7f00371d8e
Marcus Kellerman <sharkus@gmail.com>
2023-06-24 18:53:47 -0700

    Removed URI tests from AssetDetailScreen

--- ttsmutility/screens/AssetDetailScreen.py
+++ ttsmutility/screens/AssetDetailScreen.py
@@ -14,62 +14,18 @@ from pathlib import Path
 ASSET_DETAIL_MD = """
 # {mod_name}
 
+
 ## URL
 [{url}]({url})
 
+
 ## Local File URI
-[{uri}]({uri})
-
-file:///C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png
-
-> file:///C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png
-
-
-`file:///C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png`
-
-
-'file:///C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png'
-
-
-<a href="file:///C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png">Plain href</a>
-
-
-> <a href="file:///C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png">Block quote href</a>
-
-
-`<a href="file:///C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png">code href</a>`
-
-
-> `<a href="file:///C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png">Blockquote code href</a>`
-
-
-<a href="file://localhost/C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png">Localhost Plain href</a>
-
-
-`<a href="file://localhost/C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png">Localhost Code href</a>`
-
-
-> <a href="file://localhost/C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png">Localhost blockquote href</a>
-
-
-> `<a href="file://localhost/C:/Program%20Files%20%28x86%29/Steam/steamapps/common/Tabletop%20Simulator/Tabletop%20Simulator_Data/Mods/Images/httpcloud3steamusercontentcomugc1816689614996344078BAAC59ECEA0935A6A7200EDF8183892F2079F39.png">Localhost blockquote code href</a>`
-
+<a href="{uri}">{uri}</a>
 
 
 ## TTS Mod Filepath
 {filename}
 
-
-`{filepath}`
-
-
-{normpath}
-
-
-{pathlib}
-
-
-"{normpath}"
 
 | Asset Details | |
 |------------------------------:|:------------------|
@@ -115,9 +71,7 @@ class AssetDetailScreen(ModalScreen):
         self.asset_detail["other_mods"] = self.asset_detail["other_mods"].join(
             ["\n- `", "`\n"]
         )
-        self.asset_detail["uri"] = self.asset_detail["uri"][6:]
-        self.asset_detail["normpath"] = os.path.normpath(self.asset_detail["filepath"])
-        self.asset_detail["pathlib"] = str(Path(self.asset_detail["filepath"]))
+        self.asset_detail["uri"] = self.asset_detail["uri"]
         return ASSET_DETAIL_MD.format(**self.asset_detail)
 
     def _validate_uri_file_link(self, text, pos):
