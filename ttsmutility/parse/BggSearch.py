7ca0bd25377aa065089f0ba9d14f41e95de9fbb8
Marcus Kellerman <sharkus@gmail.com>
2023-07-01 09:25:43 -0700

    BGG game parsing fixed and tested

--- ttsmutility/parse/BggSearch.py
+++ ttsmutility/parse/BggSearch.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
-from urllib.parse import quote_plus, urlencode
-import urllib.request
+from html import unescape
+from urllib.parse import urlencode
+from urllib.request import urlopen
 
 
 class BggSearch:
@@ -36,6 +37,12 @@ class BggSearch:
         " cs",
     ]
 
+    BGG_TEXT_FIELDS = [
+        "description",
+        "thumbnail",
+        "image",
+    ]
+
     BGG_FIELDS = [
         "yearpublished",
         "minplayers",
@@ -44,9 +51,6 @@ class BggSearch:
         "minplaytime",
         "maxplaytime",
         "minage",
-        "description",
-        "thumbnail",
-        "image",
     ]
 
     BGG_LISTS = [
@@ -124,7 +128,7 @@ class BggSearch:
             }
         )
         url = self.BGG_SEARCH % params
-        with urllib.request.urlopen(url) as f:
+        with urlopen(url) as f:
             data = f.read().decode("utf-8")
         root = ET.fromstring(data)
         return self._parse_games(root)
@@ -132,35 +136,92 @@ class BggSearch:
     def _parse_game(self, root):
         game_info = {}
         for e in root:
-            if e.tag == "boardgame":
-                game_info["id"] = e.attrib["objectid"]
+            if e.tag == "item" and e.attrib["type"] == "boardgame":
+                game_info["id"] = e.attrib["id"]
                 for d in e:
                     # Only store the primary name
-                    if (
-                        d.tag == "name"
-                        and "primary" in d.attrib
-                        and d.attrib["primary"] == "true"
-                    ):
-                        game_info["name"] = d.text
-                    if d.tag == "statistics":
+                    if d.tag == "name" and d.attrib["type"] == "primary":
+                        game_info["name"] = d.attrib["value"]
+                    elif d.tag in self.BGG_TEXT_FIELDS:
+                        game_info[d.tag] = unescape(d.text)
+                    elif d.tag in self.BGG_FIELDS:
+                        game_info[d.tag] = d.attrib["value"]
+                    elif d.tag == "statistics":
                         for s in d[0][0]:
                             if s.tag in self.BGG_STATS:
-                                game_info[s.tag] = s.text
+                                game_info[s.tag] = unescape(s.text)
                             elif s.tag in self.BGG_STATS_LISTS:
-                                # TODO
-                                pass
-                    elif d.tag in self.BGG_FIELDS:
-                        game_info[d.tag] = d.text
+                                game_info[s.tag] = []
+                                """
+                                <ranks>
+                                    <rank type="subtype" id="1" name="boardgame" friendlyname="Board Game Rank" value="168" bayesaverage="7.3665"/>
+                                    <rank type="family" id="5497" name="strategygames" friendlyname="Strategy Game Rank" value="125" bayesaverage="7.40438"/>
+                                </ranks>
+                                """
+                                for r in s:
+                                    d = {}
+                                    for key in r.attrib.keys():
+                                        d[key] = r.attrib[key]
+                                    game_info[s.tag].append(d)
                     elif d.tag in self.BGG_LISTS:
                         if d.tag in game_info:
-                            game_info[d.tag].append(d.text)
+                            game_info[d.tag].append(unescape(d.text))
                         else:
                             game_info[d.tag] = [
-                                d.text,
+                                unescape(d.text),
                             ]
-                    elif d.tag in self.BGG_POLLS:
-                        # TODO
-                        pass
+                    elif d.tag == "poll" and d.attrib["name"] in self.BGG_POLLS:
+                        """
+                        <poll name="suggested_numplayers" title="User Suggested Number of Players" totalvotes="474">
+                            <results numplayers="1">
+                                <result value="Best" numvotes="0"/>
+                                <result value="Recommended" numvotes="0"/>
+                                <result value="Not Recommended" numvotes="251"/>
+                            </results>
+                            <results numplayers="2">
+                                <result value="Best" numvotes="16"/>
+                                <result value="Recommended" numvotes="157"/>
+                                <result value="Not Recommended" numvotes="162"/>
+                            </results>
+                        </poll>
+
+                        or
+                        <poll name="suggested_playerage" title="User Suggested Player Age" totalvotes="82">
+                            <results>
+                                <result value="2" numvotes="0"/>
+                                <result value="3" numvotes="0"/>
+                            </results>
+                        </poll>
+                        """
+                        p = {}
+                        for key in d.attrib.keys():
+                            p[key] = d.attrib[key]
+                        for r in d:
+                            if r.tag == "results":
+                                if len(r.attrib) > 0:
+                                    results_key = list(r.attrib.keys())[0]
+                                    if results_key not in p:
+                                        p[results_key] = {}
+                                    if r.attrib[results_key] not in p[results_key]:
+                                        p[results_key][r.attrib[results_key]] = []
+                                    for s in r:
+                                        v = {}
+                                        if s.tag == "result":
+                                            for key in s.attrib.keys():
+                                                v[key] = s.attrib[key]
+                                        p[results_key][r.attrib[results_key]].append(v)
+                                else:
+                                    name = p["name"]
+                                    p[name] = {}
+                                    for s in r:
+                                        if s.tag == "result":
+                                            p[name][s.attrib["value"]] = {}
+                                            for key in s.attrib.keys():
+                                                if key != "value":
+                                                    p[name][s.attrib["value"]][
+                                                        key
+                                                    ] = s.attrib[key]
+                        game_info[p["name"]] = p
         return game_info
 
     def get_game_info(self, bgg_id):
@@ -171,7 +232,7 @@ class BggSearch:
             }
         )
         url = self.BGG_GAME % params
-        with urllib.request.urlopen(url) as f:
+        with urlopen(url) as f:
             data = f.read().decode("utf-8")
         root = ET.fromstring(data)
         return self._parse_game(root)
