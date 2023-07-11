57cd8cb5ae601751439b34c7e9f5ce922d13a334
Marcus Kellerman <sharkus@gmail.com>
2023-07-10 21:09:17 -0700

    Added UpdateLog message

---
+++ ttsmutility/utility/messages.py
@@ -0,0 +1,17 @@
+from textual.message import Message
+
+class UpdateLog(Message):
+    def __init__(
+        self,
+        status: str,
+        status_id: int = 0,
+        prefix=None,
+        suffix=None,
+        flush=False,
+    ):
+        super().__init__()
+        self.status = status
+        self.status_id = status_id
+        self.prefix = prefix
+        self.suffix = suffix
+        self.flush = flush
